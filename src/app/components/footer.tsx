c87ef106889e846530eb32b1d1c8cb3f32b89b60
sanjanamasam1210 <sanjanamasam1210@gmail.com>
2025-02-13 12:27:28 +0530

    few more adjustments in the footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,169 +1,17 @@
-// "use client";
-// import Image from "next/image";
-
-// export default function Footer() {
-//   return (
-//     <footer className="w-full bg-black text-[#BEC1A7]  font-manrope">
-//       <div className="container mx-auto px-4 py-4">
-//         <div className="flex flex-row justify-between items-start md:items-center mb-20">
-//           <div className="mb-6 md:mb-0">
-//             <h2 className="text-3xl font-bold mb-2">
-//               We&apos;d love to hear from you!
-//             </h2>
-//             <p>Created by Adnan Memos & Sanjana Masam</p>
-//           </div>
-//           <div className="self-end md:self-center">
-//             <button
-//               onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-//               className="mt-4 md:mt-0"
-//             >
-//               <Image
-//                 src="/above.svg"
-//                 alt="Kofi Kinara Logo 1"
-//                 width={60}
-//                 height={10}
-//               />
-//             </button>
-//           </div>
-//         </div>
-
-//         {/* Links Section */}
-//         <div className="mb-6">
-//           <h3 className=" mb-2 underline decoration-1 underline-offset-2">
-//             Contribute to our Project
-//           </h3>
-//           <div className="flex space-x-6">
-//             <a
-//               href="https://github.com"
-//               target="_blank"
-//               rel="noopener noreferrer"
-//               className="hover:underline decoration-1 underline-offset-2 flex items-center"
-//             >
-//               <span>Github</span>
-//               <Image
-//                 src="/crossarrow.svg"
-//                 alt="External link"
-//                 width={10}
-//                 height={10}
-//                 className="ml-1"
-//               />
-//             </a>
-//             <a
-//               href="https://ko-fi.com"
-//               target="_blank"
-//               rel="noopener noreferrer"
-//               className="hover:underline decoration-1 underline-offset-2 flex items-center"
-//             >
-//               <span>Buy us a Kofi</span>
-//               <Image
-//                 src="/crossarrow.svg"
-//                 alt="External link"
-//                 width={10}
-//                 height={10}
-//                 className="ml-1"
-//               />
-//             </a>
-//           </div>
-//         </div>
-
-//         {/* Newsletter Section */}
-//         <div className="mb-6 mt-12">
-//           <h3 className=" mb-2 underline decoration-1 underline-offset-2 ">
-//             Sign up for our newsletter (No spam)
-//           </h3>
-//           <form className="relative w-96">
-//             <input
-//               type="email"
-//               placeholder="Email"
-//               className="bg-transparent border-b border-[#BEC1A7] focus:outline-none  text-base py-1 w-full pr-10 placeholder-[#555555] "
-//             />
-//             <button
-//               type="submit"
-//               className="absolute right-2 top-1/2 transform -translate-y-1/2"
-//             >
-//               <Image
-//                 src="/arrow.svg" // Replace with your actual image path
-//                 alt="Submit"
-//                 width={14}
-//                 height={10}
-//               />
-//             </button>
-//           </form>
-//         </div>
-
-//         {/* Contact Section */}
-//         <div className="flex space-x-6">
-//           <a
-//             href="https://github.com"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//             className="hover:underline decoration-1 underline-offset-2 flex items-center"
-//           >
-//             <span>Instagram</span>
-//             <Image
-//               src="/crossarrow.svg"
-//               alt="External link"
-//               width={10}
-//               height={10}
-//               className="ml-1"
-//             />
-//           </a>
-//           <a
-//             href="https://ko-fi.com"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//             className="hover:underline decoration-1 underline-offset-2 flex items-center"
-//           >
-//             <span>hello@kofikinara.com</span>
-//             <Image
-//               src="/crossarrow.svg"
-//               alt="External link"
-//               width={10}
-//               height={10}
-//               className="ml-1"
-//             />
-//           </a>
-//         </div>
-
-//         <div className="flex justify-between items-end mt-28 ">
-//           {/* Left Section */}
-//           <span className="text-11xl  font-extrabold text-[#BEC1A7] leading-none">
-//             Kofi Kinara
-//           </span>
-
-//           {/* Right Section */}
-//           <div className="flex items-end space-x-16 text-lg text-[#BEC1A7]">
-//             {/* Hyderabad - India */}
-//             <span>Hyderabad - India</span>
-
-//             {/* ©2025 and Terms of Use */}
-//             <div className="flex flex-col items-end">
-//               <span>©2025</span>
-//               <a href="#" className="hover:underline">
-//                 Terms of use
-//               </a>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </footer>
-//   );
-// }
-
 "use client";
 import Image from "next/image";
 
 export default function Footer() {
   return (
     <footer className="w-full bg-black text-[#BEC1A7] font-manrope">
-      <div className="w-full mx-auto px-8 py-8">
+      <div className="w-full mx-auto px-4 pt-4 sm:px-8 sm:pt-8 pb-9">
         {/* Top Section */}
         <div className="flex flex-row justify-between items-start md:items-center mb-12 gap-5 ">
           <div>
-            <h2 className="text-2xl md:text-3xl font-bold mb-2">
+            <h2 className="text-xl sm:text-2xl md:text-3xl font-bold mb-2">
               We&apos;d love to hear from you!
             </h2>
-            <p className="text-sm md:text-base">
+            <p className=" text-xs sm:text-sm md:text-base">
               Created by Adnan Memos & Sanjana Masam
             </p>
           </div>
@@ -222,7 +70,7 @@ export default function Footer() {
         </div>
 
         {/* Newsletter Section */}
-        <div className="mb-6 mt-12">
+        <div className="mb-6 mt-16">
           <h3 className=" text-base sm:text-lg underline decoration-1 underline-offset-2 mb-2">
             Sign up for our newsletter (No spam)
           </h3>
@@ -274,7 +122,7 @@ export default function Footer() {
         </div>
 
         {/* Bottom Section */}
-        <div className="flex flex-col lg:flex-row justify-between items-center lg:items-end mt-16 space-y-6 lg:space-y-0">
+        <div className="flex flex-col lg:flex-row justify-between items-center lg:items-end mt-16 sm:mt-24 space-y-6 lg:space-y-0 ">
           {/* Left Section */}
           <span className="text-7xl sm:text-8xl md:text-9xl lg:text-11xl font-extrabold text-[#BEC1A7] leading-none text-center lg:text-left ">
             Kofi Kinara
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,50 +1,3 @@
-// import type { Metadata } from "next";
-// import { Hind, Inter, Manrope } from "next/font/google";
-// import "./globals.css";
-// import Footer from "./components/footer";
-// import Navbar from "./components/navbar";
-
-// const hind = Hind({
-//   subsets: ["latin"],
-//   weight: ["400","500","600", "700"],
-//   variable: "--font-hind",
-// });
-
-// const inter = Inter({
-//   subsets: ["latin"],
-//   weight: ["400", "700"],
-//   variable: "--font-inter",
-// });
-
-// const manrope = Manrope({
-//   subsets: ["latin"],
-//   weight: ["200","300","400","500","600", "700","800"],
-//   variable: "--font-manrope",
-// });
-
-// export const metadata: Metadata = {
-//   title: "Kofi Kinara",
-//   description: "All Hyderabad Cafes at one place",
-//   icons: {
-//     icon:'/logo.svg'}
-// };
-
-// export default function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   return (
-//     <html lang="en" className={`${hind.variable} ${inter.variable} ${manrope.variable}`}>
-//       <body className="">
-//         <Navbar />
-//         {children}
-//         <Footer />
-//       </body>
-//     </html>
-//   );
-// }
-
 import type { Metadata } from "next";
 import Footer from "./components/footer";
 import { Hind, Inter, Manrope } from "next/font/google";
@@ -95,11 +48,11 @@ export default function RootLayout({
         {/* Viewport Meta Tag (Critical for Responsiveness) */}
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
-      <body className="">
+      <body className="min-h-screen flex flex-col">
         {/* Navbar */}
         <Navbar />
         {/* Main Content */}
-        <main className="min-h-screen">{children}</main>
+        <main className="flex-grow">{children}</main>
         {/* Footer */}
         <Footer />
       </body>
